43fdaa8cfb40e6067fb5ec95548e32f3bec658ff
Alex McCaskey <mccaskeyaj@ornl.gov>
2020-10-01 09:11:38 -0400

    adding a gitlab ci config file
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- docker/xacc/debian/bionic/build.sh
+++ docker/xacc/debian/bionic/build.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+# Build the deb via Docker
 docker build -t xacc/bionic . --no-cache
 docker run -d -P -it --name bionicxacc xacc/bionic
 id=$(docker ps -aqf "name=bionicxacc") && docker cp $id:/home/dev/xacc/build/xacc-1.0.0.deb . 
@@ -10,3 +11,12 @@ docker rm bionicxacc
 
 # git clone the pages repo
 # upload the xacc deb file there
+git clone https://github.com/aide-qc/deploy 
+cd deploy
+cp ../xacc-1.0.0.deb xacc/debian/bionic 
+git status
+git add -A 
+git commit -s -m "automated ci build of xacc bionic deb"
+git config remote.aideqcdeploy.url >&- || git remote add -t master aideqcdeploy https://amccaskey:$AIDEQC_ACCESS_TOKEN@github.com/aide-qc/deploy
+git push -f aideqcdeploy HEAD:master
+git remote remove aideqcdeploy
