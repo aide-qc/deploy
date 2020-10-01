61e8bf0c181d4c5778a307e6c23be17059edec01
Alex McCaskey <mccaskeyaj@ornl.gov>
2020-10-01 15:52:35 -0400

    adding clang syntax handler deb for bionic
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

---
+++ docker/clang_syntax_handler/debian/bionic/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+
+# Build the deb via Docker
+docker build -t csp/bionic .
+docker run -d -P -it --name csp csp/bionic
+id=$(docker ps -aqf "name=csp") && docker cp $id:/llvm/build/LLVM-10.0.0git-Linux.deb . 
+docker stop csp 
+docker rm csp
