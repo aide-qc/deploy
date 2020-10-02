9fc6f0f5bafd65fdf138883771de86601c4d1b27
Alex McCaskey <mccaskeyaj@ornl.gov>
2020-10-02 15:02:16 -0400

    adding docker build files for csp
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- docker/clang_syntax_handler/debian/bionic/build.sh
+++ docker/clang_syntax_handler/debian/bionic/build.sh
@@ -3,7 +3,7 @@
 set -x
 
 # Build the deb via Docker
-docker build -t csp/bionic .
+docker build -t csp/bionic . --no-cache
 docker run -d -P -it --name csp csp/bionic
 id=$(docker ps -aqf "name=csp") && docker cp $id:/llvm/build/LLVM-10.0.0git-Linux.deb . 
 docker stop csp 
---
+++ docker/clang_syntax_handler/debian/focal/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+
+# Build the deb via Docker
+docker build -t csp/focal . --no-cache
+docker run -d -P -it --name csp csp/focal
+id=$(docker ps -aqf "name=csp") && docker cp $id:/llvm/build/LLVM-10.0.0git-Linux.deb . 
+docker stop csp 
+docker rm csp
