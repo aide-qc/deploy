6492a8432334859fbc1200ffe3dc8a327b0e5950
Alex McCaskey <mccaskeyaj@ornl.gov>
2020-11-24 14:38:35 -0500

    adding install code for Rigetti QCS
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- install.sh
+++ install.sh
@@ -5,6 +5,79 @@ if [ "$1" == "--use-brew" ]; then
    export use_brew=true
 fi
 
+if [ "$1" == "-qcs" ]; then
+    # This is for installing aide-qc stack on 
+    # Rigetti QCS JupyterLab IDE. Does not require sudo :)
+    
+    # pull down llvm and xacc deb packages
+    wget https://raw.githubusercontent.com/aide-qc/deploy/master/xacc/debian/focal/xacc-1.0.0.deb    
+    wget https://raw.githubusercontent.com/aide-qc/deploy/master/clang_syntax_handler/debian/focal/LLVM-10.0.0git-Linux.deb
+    dpkg -x xacc-1.0.0.deb $HOME/.aideqc_install 
+    dpkg -x LLVM-10.0.0git-Linux.deb $HOME/.aideqc_install 
+    
+    python3 -m pip install cmake ipopo --user 
+    export MY_CWD=$PWD
+    git clone https://github.com/eclipse/xacc
+    cd xacc/quantum/plugins/rigetti/qcs 
+    cp ../accelerator/QuilVisitor.hpp .
+    mv CMakeLists.standalone.txt CMakeLists.txt
+    mkdir tpls && cd tpls
+
+    wget https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.gz
+    tar -xzvf boost_1_74_0.tar.gz && cd boost_1_74_0
+    ./bootstrap.sh --prefix=$HOME/.boost
+    ./b2 --with-system --with-chrono install
+    cd ..
+
+    # msgpack
+    git clone -b cpp_master https://github.com/msgpack/msgpack-c
+    cd msgpack-c && mkdir build && cd build
+    CC=gcc CXX=g++ cmake .. -DCMAKE_INSTALL_PREFIX=~/.zmq -DBOOST_ROOT=$HOME/.boost
+    make -j4 install
+    cd ../../
+
+    #cppzmq
+    git clone https://github.com/zeromq/cppzmq
+    cd cppzmq/ && mkdir build && cd build/
+    CC=gcc CXX=g++ cmake .. -DCMAKE_INSTALL_PREFIX=~/.zmq \
+                            -DCMAKE_PREFIX_PATH=~/.zmq \
+                            -DCPPZMQ_BUILD_TESTS=FALSE
+    make -j12 install
+    cd ../../
+
+    # qcs
+    cd .. && mkdir build && cd build
+    CC=gcc CXX=g++ cmake .. -DXACC_DIR=~/.aideqc_install/usr/local/xacc -DBOOST_ROOT=$HOME/.boost
+    make -j4 install
+    
+    cd $MY_CWD && rm -rf xacc
+    git clone https://github.com/ornl-qci/qcor
+    cd qcor && mkdir build && cd build
+    CC=gcc CXX=g++ cmake .. -DLLVM_ROOT=$HOME/.aideqc_install/usr/local/xacc/llvm \
+                            -DXACC_DIR=$HOME/.aideqc_install/usr/local/xacc \
+                            -DCMAKE_CXX_FLAGS="-D__STDC_FORMAT_MACROS" \
+                            -DCMAKE_INSTALL_PREFIX=$HOME/.qcor
+    make -j4 install
+    cd ../../ && rm -rf qcor *.deb 
+    
+    echo ""
+    echo "AIDE-QC installed on Rigetti QCS."
+    echo ""
+    echo "Your XACC install location is "
+    echo "$HOME/.aideqc_install/usr/local/xacc"
+    echo ""
+    echo "Your QCOR install location is "
+    echo "$HOME/.qcor"
+    echo ""
+    echo "Export your PATH to include the qcor binary executable location:"
+    echo "export PATH=\$PATH:$HOME/.qcor/bin"
+    echo ""
+    echo "To use the Python API, please run the following (and add to your .bashrc or .bash_profile)"
+    echo "export PYTHONPATH=\$PYTHONPATH:$HOME/.aideqc_install/usr/local/xacc:$HOME/.qcor"
+    echo ""
+    exit 0
+fi
+
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=false
 UNAME=$(uname | tr "[:upper:]" "[:lower:]")
 # If Linux, try to determine specific distribution
